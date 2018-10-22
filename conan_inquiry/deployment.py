3fb296e89c55ca55559ece668cdc7d414dd80f0a
Croydon <cr0ydon@outlook.com>
2018-10-23 00:45:10 +0200

    Don't ask me about this in a job interview

--- conan_inquiry/deployment.py
+++ conan_inquiry/deployment.py
@@ -1,9 +1,6 @@
-from datetime import datetime
-from tempfile import TemporaryDirectory
-
 import os
 import subprocess
-import hashlib
+from shutil import copy
 
 from conan_inquiry.web.file_retrieval import WebFiles
 
@@ -17,7 +14,9 @@ def deploy():
 
         print('* Running: git', subcmd)
         retcode = subprocess.call(['git', subcmd] + list(args), **kwargs)
-        if retcode != 0:
+
+        # Something went wrong, commit is allowed to fail in this context as we might have nothing new to commit
+        if retcode != 0 and subcmd != "commit":
             raise ChildProcessError('Unable to run the command')
 
     repository = os.getenv('GITHUB_REPO', 'https://{}@github.com/Croydon/conan_inquiry'.format(os.getenv('GITHUB_TOKEN')))
@@ -28,13 +27,27 @@ def deploy():
     else:
         git('pull')
 
+    if not os.path.exists('build'):
+        os.makedirs('build')
+
+    for file in os.listdir(os.path.join(os.path.dirname(__file__), 'web', 'files')):
+        print("copy:")
+        print(os.path.join(os.path.dirname(__file__), 'web', 'files', file))
+        copy(os.path.join(os.path.dirname(__file__), 'web', 'files', file), 'build')
+
     files = WebFiles()
 
-    for file in files.names():
-        with open(os.path.join(dir, 'conan_inquiry', file), 'wt') as f:
-            f.write(files.get_file(file, debug=False))
-        git('add', os.path.join(dir, 'conan_inquiry', file))
+    for file in files.deploy_names():
+        file_path = os.path.join(os.path.dirname(__file__), '..', 'build', file)
 
+        with open(file_path, 'r+', encoding="utf8") as f:
+            data = files.adjust_content(file, f.read(), debug=False)
+            f.seek(0)
+            f.write(data)
+            f.truncate()
+        copy(file_path, os.path.join('gh-pages', 'conan_inquiry'))
+
+    git('add', '-A')
     git('commit', '-am', 'Automatic deployment')
     git('push')
     print('* Deployment successful!')
--- conan_inquiry/web/file_retrieval.py
+++ conan_inquiry/web/file_retrieval.py
@@ -11,43 +11,71 @@ class WebFiles:
     replacement_html_re = re.compile(r'<import +href="([^"]+)"(/>|></import>)')
     replacement_js_re = re.compile(r'//import (\S+)')
     dir = os.path.join(os.path.dirname(__file__), 'files')
+    # FIXME: Add  'packages.js', 'packages.json', to list after package upadates are working to deploy them
+    files_generated = ['wishlist.json', 'wishlist.js']
 
+    # Returns full path to a file
     def full_name(self, name):
         if self.is_constant(name):
             return os.path.join(os.path.dirname(__file__), '..', '..', 'build', name)
         return os.path.join(self.dir, name)
 
+    # Check if file exists
     def exists(self, name):
         return os.path.exists(self.full_name(name))
 
-    def get_file(self, name: str, debug=False):
-        if not self.exists(name):
-            return None
-
-        with open(self.full_name(name), 'r') as f:
-            result = f.read()
-
-        result = self.replacement_html_re.sub(lambda x: self.get_file(x.group(1)), result)
-        result = self.replacement_js_re.sub(lambda x: self.get_file(x.group(1)), result)
-        if not debug:
-            result = result.replace('CACHE_BUSTER',
-                                    hashlib.md5(datetime.now().isoformat().encode('utf-8')).hexdigest())
-
-        if name.endswith('.js'):
-            result = jsmin(result)
+    def minimize_content(self, input, format):
+        result = ''
+        if format == 'js':
+            result = jsmin(input)
 
         return result
 
+    # Returns content of a file, minimized and adjusted
+    def adjust_content(self, name: str, content, debug=False):
+        content = self.replacement_html_re.sub(lambda x: self.get_file(self.full_name(x.group(1))), content)
+        content = self.replacement_js_re.sub(lambda x: self.get_file(self.full_name(x.group(1))), content)
+        if not debug:
+            content = content.replace('CACHE_BUSTER',
+                                    hashlib.md5(datetime.now().isoformat().encode('utf-8')).hexdigest())
+
+        if name.endswith('.js'):
+            content = self.minimize_content(content, 'js')
+
+        return content
+
+    # Returns content of a file, minimized and adjusted
+    def get_file(self, name: str, debug=False):
+        with open(name, 'r', encoding="utf8") as f:
+            result = f.read()
+            result = self.adjust_content(name, result, debug)
+
+        return result
+
+    # Returns file size
     def size(self, name):
         return os.path.getsize(self.full_name(name))
 
-    def names(self):
-        files = ['packages.js', 'packages.json', 'wishlist.json', 'wishlist.js']
+    # Returns list of file names which are important for web deployment
+    def deploy_names(self):
+        files = self.files_generated
         for entry in os.listdir(self.dir):
-            full_name = os.path.join(self.dir, entry)
-            if os.path.isfile(full_name) and not entry.startswith('_'):
+            if os.path.isfile(os.path.join(self.dir, entry)) and not entry.startswith('_'):
                 files.append(entry)
         return files
 
+    # required?
+    def full_names(self):
+        files = []
+        for file_gen in self.files_generated:
+            if os.path.isfile(self.full_name(file_gen)):
+                files.append(self.full_name(file_gen))
+        for entry in os.listdir(self.dir):
+            if os.path.isfile(self.full_name(entry)) and not entry.startswith('_'):
+                files.append(self.full_name(entry))
+        return files
+
     def is_constant(self, name):
-        return name == 'packages.js' or name == 'packages.json' or name == 'wishlist.json' or name == 'wishlist.js'
+        if name in self.files_generated:
+            return True
+        return False
